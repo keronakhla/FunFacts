d03dedd4b3cb2bf3b5cb212add98738c557b1e2d
Kerolos <keronakhla@gmail.com>
2015-09-23 22:48:50 -0700

    improving UI design

---
+++ FunFacts/FunFacts/ColorWheel.h
@@ -0,0 +1,14 @@
+//
+//  ColorWheel.h
+//  FunFacts
+//
+//  Created by Kerolos Nakhla on 9/23/15.
+//  Copyright (c) 2015 Treehouse. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface ColorWheel : NSObject
+
+@end
---
+++ FunFacts/FunFacts/ColorWheel.m
@@ -0,0 +1,13 @@
+//
+//  ColorWheel.m
+//  FunFacts
+//
+//  Created by Kerolos Nakhla on 9/23/15.
+//  Copyright (c) 2015 Treehouse. All rights reserved.
+//
+
+#import "ColorWheel.h"
+
+@implementation ColorWheel
+
+@end
